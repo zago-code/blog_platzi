fdb1395942c18818f1baa1136e50d276e287ea94
Gabriel Zarate <gzaratet1@gmail.com>
2020-07-23 00:28:53 -0500

    Agregando estado al componente con Stateful

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,63 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const App = () => {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      usuarios:[
+        {
+          nombre: 'Gabriel',
+          correo: 'gabriel@zarate.com',
+          enlace: 'gabriel.com'
+        },
+        {
+          nombre: 'Platzi',
+          correo: 'platzi@platzi.com',
+          enlace: 'platzi.com'
+        }
+      ]
+    }
+  }
+  ponerFilas = () => (
+    this.state.usuarios.map((usuario) => (
+      <tr>
+        <td>
+          { usuario.nombre }
+        </td>
+        <td>
+          { usuario.correo }
+        </td>
+        <td>
+          { usuario.enlace }
+        </td>
+      </tr>
+    ))
+  );
 
-  const ponerFilas = () => [
-    <tr>
-      <td>
-        Gabriel
-      </td>
-      <td>
-        gabriel@zarate.com
-      </td>
-      <td>
-        gabriel.com
-      </td>
-    </tr>,
-    <tr>
-      <td>
-        Platzi
-      </td>
-      <td>
-        platzi@platzi.com
-      </td>
-      <td>
-        platzi.com
-      </td>
-    </tr>
-  ];
-
-  return(
-    <div className="margen">
-      <table className="tabla">
-        <thead>
-          <tr>
-            <th>
-              Nombre
-            </th>
-            <th>
-              Correo
-            </th>
-            <th>
-              Enlace
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {ponerFilas()}
-        </tbody>
-      </table>
-    </div>
-  )
+  render() {
+    return(
+      <div className="margen">
+        <table className="tabla">
+          <thead>
+            <tr>
+              <th>
+                Nombre
+              </th>
+              <th>
+                Correo
+              </th>
+              <th>
+                Enlace
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.ponerFilas()}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
 
 export default App;
