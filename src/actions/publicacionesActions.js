865689f0a7bb93106aab5eae15967a4f8633f0b2
Gabriel Zarate <gzaratet1@gmail.com>
2020-08-17 01:42:07 -0500

    Evitar segundas busquedas, Inmutabilidad y evitando sobre escritura

--- src/actions/publicacionesActions.js
+++ src/actions/publicacionesActions.js
@@ -1,38 +1,39 @@
 import axios from 'axios';
-import { TRAER_TODOS, CARGANDO, ERROR } from '../types/publicacionesTypes';
+import { TRAER_POR_USUARIO, CARGANDO, ERROR } from '../types/publicacionesTypes';
+import * as usuariosTypes from '../types/usuariosTypes';
 
-export const traerTodos = () => async dispatch => {
+const { TRAER_TODOS: USUARIOS_TRAER_TODOS } = usuariosTypes;
+
+export const traerPorUsuario = key => async (dispatch, getState) => {
   dispatch({
     type: CARGANDO,
   });
-  const respuesta = await axios.get('https://jsonplaceholder.typicode.com/posts');
+  const { usuarios } = getState().usuariosReducer;
+  const { publicaciones } = getState().publicacionesReducer;
+  const usuario_id = usuarios[key].id;
+  const respuesta = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${usuario_id}`);
+  const publicaciones_actualizadas = [...publicaciones, respuesta.data];
+  const publicaciones_key = publicaciones_actualizadas.length - 1;
+  const usuarios_actualizados = [...usuarios];
+  usuarios_actualizados[key] = {
+    ...usuarios[key],
+    // publicaciones_key: publicaciones_key
+    publicaciones_key,
+  };
+  dispatch({
+    type: USUARIOS_TRAER_TODOS,
+    payload: usuarios_actualizados,
+  });
   try {
     dispatch({
-      type: TRAER_TODOS,
-      payload: respuesta.data,
+      type: TRAER_POR_USUARIO,
+      payload: publicaciones_actualizadas,
     });
   } catch (error) {
+    console.log('Error: ', error.message);
     dispatch({
       type: ERROR,
       payload: error.message,
     });
-  }
-};
-
-export const traerPorUsuario = key => async (dispatch, getState) => {
-  const { usuarios } = getState().usuariosReducer;
-  const usuario_id = usuarios[key].id;
-  const respuesta = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${usuario_id}`);
-  try {
-    dispatch({
-      type: TRAER_TODOS,
-      payload: respuesta.data,
-    });
-  } catch (error) {
-    console.log('Error: ', error.message);
-    // dispatch({
-    //   type: ERROR,
-    //   payload: error.message,
-    // });
   }
 };
